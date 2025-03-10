225668934cc396acd597ffef3cd65f3d50e58530
Kenny Gourdier <kennethgourdier@gmail.com>
2025-03-10 16:47:18 -0400

    fill,while running loop

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pygame
-import constants 
+from constants import *  
 
 def main():
-    print('Starting Asteroids!')
-    print('Screen width:',constants.SCREEN_WIDTH)
-    print('Screen height:',constants.SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
+        screen.fill(black) 
+        pygame.display.flip()
 
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
