63a2211f5a8d08c8a0332078e5f0624bb17850e9
Kenny Gourdier <kennethgourdier@gmail.com>
2025-03-12 04:07:36 -0400

    player and rotation

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pygame
 from constants import *  
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while running:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill(black) 
-        pygame.display.flip()
+        player.update(dt)
+        screen.fill(BLACK) 
+        player.draw(screen)
 
+
+        pygame.display.flip()
+       
     pygame.quit()
 
 if __name__ == "__main__":
